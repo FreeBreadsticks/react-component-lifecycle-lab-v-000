7a860651f721098141401ca04277517aef027e94
FreeBreadsticks <erika.hughes@dbspublishing.com>
2017-10-20 16:19:51 -0400

    Done.

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -11,8 +11,30 @@ class TweetWall extends React.Component {
   }
 
   // TODO: componentWillMount()
+  componentWillMount = () => {
+    this.setState({
+      tweets: this.props.newTweets,
+    });
+  }
   // TODO: shouldComponentUpdate()
+  shouldComponentUpdate = (nextProps, nextState) => {
+    if (nextProps.newTweets.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
   // TODO: componentWillReceiveProps()
+  componentWillReceiveProps = (nextProps) => {
+    var currentTweets = this.state.tweets
+    nextProps.newTweets.map(tweet =>{
+      currentTweets.unshift(tweet);
+    });
+    this.setState({
+      tweets: currentTweets,
+    })
+
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
